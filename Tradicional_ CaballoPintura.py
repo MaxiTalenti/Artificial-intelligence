f9c68f3418321f0c0c2b18289f0054ae56c132bd
Maximiliano  Talenti <maximiliano.talenti@gmail.com>
2017-02-10 01:34:24 -0300

    Comenzando ejercicios.

--- Tradicional_ CaballoPintura.py
+++ Tradicional_ CaballoPintura.py
@@ -1,4 +1,5 @@
-# Supongamos que tenemos un tablero rectangular dividido en cuadrados, cuyo tamaño es 3 cuadrados de largo por 4 de ancho. Inicialmente, todos los cuadrados están pintados de blanco
+# Supongamos que tenemos un tablero rectangular dividido en cuadrados, cuyo tamaño es 3 cuadrados de largo por 4 de ancho.
+#  Inicialmente, todos los cuadrados están pintados de blanco
 # | X |   |   |   |	 
 # |   |   |   |   |	 
 # |   |   |   |   |	 
@@ -11,3 +12,42 @@
 # # y lo pinta de color rojo. 
 # - Inicialmente, el robot se encuentra sobre el cuadrado de la esquina superior izquierda, que ya está pintado de rojo.
 
+# coding=utf-8
+from simpleai.search import SearchProblem, breadth_first, depth_first, greedy, astar
+from simpleai.search.viewers import ConsoleViewer, BaseViewer
+import random
+
+ESTADO = [(0,1), (0,2), (0,3), (1,0), (1,1), (1,2), (1,3), (2,0), (2,1), (2,2), (2,3)]
+
+class Problema(SearchProblem):
+
+    def is_goal(self, state):
+        return len(state) == 1
+
+    def result(self, state, actions):
+
+    def actions(self, state):
+
+    def cost(self, state1, action, state2):
+        return 1
+
+    def heuristic(self, state):
+        return len(state)
+
+
+def resolver(metodo_busqueda,posicion_rey,controlar_estados_repetidos):
+	problema = Problema(posicion_rey)
+	visor = BaseViewer()
+	#Busquedas, Grafo -> graph_search=True
+	if (metodo_busqueda == 'breadth_first'): # En amplitud
+		resultado = breadth_first(problema, graph_search=controlar_estados_repetidos, viewer=visor)
+	elif (metodo_busqueda == 'depth_first'): # Profundidad
+		resultado = depth_first(problema, graph_search=controlar_estados_repetidos, viewer=visor)
+	elif (metodo_busqueda == 'greedy'): # Avara
+		resultado = greedy(problema, graph_search=controlar_estados_repetidos, viewer=visor)
+	elif (metodo_busqueda == 'astar'): # Estrella
+		resultado = astar(problema, graph_search=controlar_estados_repetidos, viewer=visor)
+	print(visor.stats)
+	return resultado
+
+resolver(metodo_busqueda='astar', posicion_rey=ESTADO, controlar_estados_repetidos=True)
