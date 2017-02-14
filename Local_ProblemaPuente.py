52db37c24f0314660e35bf068b35c18f0b48cc83
Talenti Maximiliano <mtalenti@sancorseguros.com>
2017-02-14 15:33:27 -0300

    Fixed
    Se soluciono el problema con RataBots y se hizo el ProblemaPuente (Ver
    heuristica).

--- Local_ProblemaPuente.py
+++ Local_ProblemaPuente.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # Un grupo de 5 personas quiere cruzar un viejo y estrecho puente. 
 # Es una noche cerrada y se necesita llevar una linterna para cruzar. 
 # El grupo sólo dispone de una linterna, a la que le quedan 5 minutos de batería. 
@@ -8,3 +9,87 @@
 # alguien tiene que volver a cruzar hacia atrás con la linterna a buscar a los compañeros que faltan, 
 # y así hasta que hayan cruzado todos.
 
+from simpleai.search import SearchProblem, breadth_first, depth_first, greedy, astar
+from simpleai.search.viewers import ConsoleViewer, BaseViewer
+
+VALORES = {1: 10, 2: 30, 3: 60, 4: 80, 5: 120}
+ESTADO = ('A', #Linterna
+		  (1,2,3,4,5), # Personas en A
+		  ()) # Personas en B
+
+class Problema(SearchProblem):
+
+    def is_goal(self, state):
+    	linterna, a, b = state
+    	return len(a) == 0 and len(b) == 5
+
+    def actions(self, state):
+    	linterna, a, b = state
+    	acciones = []
+    	if linterna == 'A':
+    		for x in a:
+    			for y in a:
+    				if x != y:
+    					acciones.append(('Mover', (x,y)))
+    	else:
+    		for x in b:
+    			acciones.append(('Volver', x))
+
+    	return acciones
+
+    def result(self, state, action):
+    	linterna, a, b = state
+    	mov, z = action
+    	alist = list(a)
+    	blist = list(b)
+    	if mov == 'Mover':
+    		x, y = z
+    		alist.remove((x))
+    		alist.remove((y))
+    		blist.append((x))
+    		blist.append((y))
+    		linterna = 'B'
+    	else:
+    		alist.append((z))
+    		blist.remove((z))
+    		linterna = 'A'
+
+    	return linterna, tuple(alist), tuple(blist)
+
+	def cost(self, state1, action, state2):
+		mov, a = action
+        if mov == 'Mover':
+        	x,y = a
+        	return max(VALORES[x], VALORES[y])
+        else:
+        	return VALORES[a]
+
+    def heuristic(self, state):
+    	linterna, a, b = state
+    	sincruzar = len(a)
+    	if linterna == 'A':
+    		return sincruzar + (sincruzar -1)
+    	else:
+    		if sincruzar == 0:
+    			return 0
+    		else:
+    			return sincruzar * 2
+
+def resolver(metodo_busqueda,posicion_rey,controlar_estados_repetidos):
+	problema = Problema(posicion_rey)
+	visor = BaseViewer()
+	#Busquedas, Grafo -> graph_search=True
+	if (metodo_busqueda == 'breadth_first'): # En amplitud
+		resultado = breadth_first(problema, graph_search=controlar_estados_repetidos, viewer=visor)
+	elif (metodo_busqueda == 'depth_first'): # Profundidad
+		resultado = depth_first(problema, graph_search=controlar_estados_repetidos, viewer=visor)
+	elif (metodo_busqueda == 'greedy'): # Avara
+		resultado = greedy(problema, graph_search=controlar_estados_repetidos, viewer=visor)
+	elif (metodo_busqueda == 'astar'): # Estrella
+		resultado = astar(problema, graph_search=controlar_estados_repetidos, viewer=visor)
+	print(resultado.state)
+	for a in resultado.path():
+		print 'Step', a
+	return resultado
+
+resolver(metodo_busqueda='astar', posicion_rey=ESTADO, controlar_estados_repetidos=True)
--- Tradicional_RataBots.py
+++ Tradicional_RataBots.py
@@ -20,55 +20,48 @@ from simpleai.search.viewers import ConsoleViewer, BaseViewer
 
 FINAL = (5,3) # Casilla por la cual debe salir
 
+PAREDES = ((0,2), (1,1), (1,4), (2,3), (3,0), (3,1), (3,4), (3,5), (4,3), (5,1), (5,5))
+
 ESTADO = ((5,2), # Robot
-         ((0,2), (1,1), (1,4), (2,3), (3,0), (3,1), (3,4), (3,5), (4,3), (5,1), (5,5)), # Paredes
-         ((0,1), (2,4), (4,2)), # Comidas
-         ((0,0), (0,3), (0,4), (0,5), (1,0), (1,2), (1,3), (1,5), (2,0), (2,1), (2,2), (2,5), (3,2), (3,3),
-         (4,0), (4,1), (4,4), (4,5), (5,0), (5,3), (5,4))) # Espacios libres
+         ((0,1), (2,4), (4,2))) # Comidas
 
 class Problema(SearchProblem):
 
     def is_goal(self, state):
-        robot, paredes, comidas, esplibres = state
-        if len(comidas) == 0:
-            print 'comidas vacias', robot, comidas
+        robot, comidas = state
         return (robot == FINAL and len(comidas) == 0)
 
     def actions(self, state):
-        robot, paredes, comidas, esplibres = state
+        robot, comidas = state
         x, y = robot
         acciones = []
-        if ((x+1,y) in esplibres) or ((x+1,y) in comidas):
-            acciones.append(('Derecha', (x+1,y)))
-        if ((x-1,y) in esplibres) or ((x-1,y) in comidas):
-            acciones.append(('Izquierda', (x-1,y)))
-        if ((x,y-1) in esplibres) or ((x,y-1) in comidas):
-            acciones.append(('Abajo', (x,y-1)))
-        if ((x,y+1) in esplibres) or ((x,y+1) in comidas):
-            acciones.append(('Arriba', (x,y+1)))
+
+        if x < 5 and (x+1,y) not in PAREDES:
+        	acciones.append(('Derecha', (x+1,y)))
+        if x > 0 and (x-1,y) not in PAREDES:
+        	acciones.append(('Izquierda', (x-1,y)))
+        if y < 5 and (x,y+1) not in PAREDES:
+        	acciones.append(('Arriba', (x,y+1)))
+        if y > 0 and (x,y-1) not in PAREDES:
+        	acciones.append(('Abajo', (x,y-1)))
         
         return acciones
 
     def result(self, state, action):
-        robot, paredes, comidas, espacioslibres = state
+        robot, comidas = state
         x, y = action[1]
         com = list(comidas)
-        espl = list(espacioslibres)
         if (x,y) in comidas:
             # Comio
             com.remove((x,y))
-        else:
-            # Movio
-            espl.remove((x,y))
-        espl.append((robot))
 
-        return ((x,y), paredes, tuple(com), tuple(espl))
+        return ((x,y), tuple(com))
 
     def cost(self, state1, action, state2):
         return 1
 
     def heuristic(self, state):
-        robot, paredes, comidas, espacioslibres = state
+        robot, comidas = state
         x, y = robot
         a, b = FINAL
         z = max([abs(x-a), abs(y-b)])
