1c99c85057f9252e4abd32ad750d24530bac1bec
Talenti Maximiliano <mtalenti@sancorseguros.com>
2017-03-01 15:33:21 -0300

    Fixed
    Se corrigió el parcial.

--- Parcial1.py
+++ Parcial1.py
@@ -3,8 +3,6 @@ from simpleai.search import SearchProblem, breadth_first, depth_first, greedy, a
 from simpleai.search.viewers import ConsoleViewer, BaseViewer, WebViewer
 import random
 
-pintados = []
-
 class problem(SearchProblem):
     def is_goal(self, state):
         for a in state:
@@ -13,23 +11,29 @@ class problem(SearchProblem):
         return True
 
     def actions(self, state):
-        print 'actions'
         acciones = []
         for a in state:
-            acciones.append((a[0],a[1]))
+            if a[2] == 0:
+                acciones.append((a[0],a[1]))
         return acciones
 
-    def results(self, state, actions):
+    def result(self, state, actions):
         a,b = actions
         statenuevo = list(state)
-        statenuevo.remove(actions)
+        statenuevo.remove((a,b,0))
         statenuevo.append((a,b,1))
         if (a-1,b,0) in state:
-            statenuevo.remove(a-1,b,0)
-            statenuevo.append(a-1,b,1)
+            statenuevo.remove((a-1,b,0))
+            statenuevo.append((a-1,b,1))
         if (a,b-1,0) in state:
-            statenuevo.remove(a,b-1,0)
-            statenuevo.remove(a,b-1,1)
+            statenuevo.remove((a,b-1,0))
+            statenuevo.append((a,b-1,1))
+        if (a+1,b,0) in state:
+            statenuevo.remove((a+1,b,0))
+            statenuevo.append((a+1,b,1))
+        if (a,b+1,0) in state:
+            statenuevo.remove((a,b+1,0))
+            statenuevo.append((a,b+1,1))
         return tuple(statenuevo)
 
     def heuristic(self, state):
@@ -50,7 +54,10 @@ def resolver(metodo_busqueda,initial,controlar_estados_repetidos):
 		resultado = greedy(problema, graph_search=controlar_estados_repetidos, viewer=visor)
 	elif (metodo_busqueda == 'astar'): # Estrella
 		resultado = astar(problema, graph_search=controlar_estados_repetidos, viewer=visor)
-	print(visor.stats)
 	return resultado
 
-resolver('breadth_first', ((0,0,0),(0,1,0),(0,2,0),(1,0,0),(1,1,0),(1,2,0),(2,0,0),(2,1,0),(2,2,0)), True)
+result = resolver('astar', ((0,0,0),(0,1,0),(0,2,0),(1,0,0),(1,1,0),(1,2,0),(2,0,0),(2,1,0),(2,2,0)), True)
+print result
+for action, state in result.path():
+    print 'action', action
+    print 'result', result
